c86c0a3b38e491c62dfd665fcdeaa54c4b2e3b89
ValeriyH <ValeriyH@users.noreply.github.com>
2018-06-15 14:56:59 +0300

    WebApi trainings

---
+++ WebAPI/WebAPI.Tests/Controllers/ValuesControllerTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebAPI;
+using WebAPI.Controllers;
+
+namespace WebAPI.Tests.Controllers
+{
+    [TestClass]
+    public class ValuesControllerTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            // Arrange
+            PaymentController controller = new PaymentController();
+
+            // Act
+            IEnumerable<string> result = controller.Get();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("value1", result.ElementAt(0));
+            Assert.AreEqual("value2", result.ElementAt(1));
+        }
+
+        [TestMethod]
+        public void GetById()
+        {
+            // Arrange
+            PaymentController controller = new PaymentController();
+
+            // Act
+            string result = controller.Get(5);
+
+            // Assert
+            Assert.AreEqual("value", result);
+        }
+
+        [TestMethod]
+        public void Post()
+        {
+            // Arrange
+            PaymentController controller = new PaymentController();
+
+            // Act
+            controller.Post("value");
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Put()
+        {
+            // Arrange
+            PaymentController controller = new PaymentController();
+
+            // Act
+            controller.Put(5, "value");
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            // Arrange
+            PaymentController controller = new PaymentController();
+
+            // Act
+            controller.Delete(5);
+
+            // Assert
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Models/Data.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace WebAPI.Models
+{
+    public class Payment
+    {
+        public DateTime time { get; set; }
+        public double payment { get; set; }
+    }
+
+    public class PaymentStorage
+    {
+        List<Payment> _data = new List<Payment>();
+
+        private void Load()
+        {
+            string file = ConfigurationManager.AppSettings["StoragePath"];
+            if (File.Exists(file))
+            {
+                _data = JsonConvert.DeserializeObject<List<Payment>>(File.ReadAllText(file));
+            }
+            else
+            {
+                _data = new List<Payment>();
+            }
+        }
+        private void Save()
+        {
+            string file = ConfigurationManager.AppSettings["StoragePath"];
+            File.WriteAllText(file, JsonConvert.SerializeObject(_data));
+        }
+        public PaymentStorage()
+        {
+            Load();
+        }
+
+        public int AddPayment(Payment value)
+        {
+            _data.Add(value);
+            Save();
+            return _data.Count;
+        }
+
+        public IEnumerable<Payment> GetEnumerator()
+        {
+            return _data.AsEnumerable();
+        }
+
+        public Payment GetPayment(int id)
+        {
+            return _data[id];
+        }
+
+        public void UpdatePayment(int id, Payment value)
+        {
+            _data[id] = value;
+            Save();
+        }
+
+        public void RemovePayment(int id)
+        {
+            _data.RemoveAt(id);
+        }
+    }
+}
