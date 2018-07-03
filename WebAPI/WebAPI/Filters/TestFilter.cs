d6e3214cdbf569e3b39fbd68890392a44d94255b
ValeriyH <ValeriyH@users.noreply.github.com>
2018-07-03 18:10:44 +0300

    Add Filters

---
+++ WebAPI/WebAPI/Filters/TestFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace WebAPI.Filters
+{
+    public class TestFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            var test = actionContext;
+            //if (actionContext.ActionArguments["shift"] != 0)
+            //{
+            //    actionContext.ActionArguments["id"] = actionContext.ActionArguments["shift"];
+            //}
+        }
+    }
+}
